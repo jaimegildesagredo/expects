c3bfa4b8985a9fd80f1bd77c042aa84f117dcede
Jaime Gil de Sagredo Luna <jaimegildesagredo@gmail.com>
2014-02-23 19:27:58 +0100

    Unnecessary outer describe

--- spec/testing/failure_spec.py
+++ spec/testing/failure_spec.py
@@ -6,52 +6,51 @@ from expects import expect
 from expects.testing import failure
 
 
-with describe('testing') as _:
-    with describe('failure'):
-        def it_should_pass_if_assertion_error_raised_and_message_matches():
-            def callback():
-                with failure(_.actual, _.message):
-                    raise AssertionError('Expected {!r} {}'.format(_.actual,  _.message))
+with describe('failure') as _:
+    def it_should_pass_if_assertion_error_raised_and_message_matches():
+        def callback():
+            with failure(_.actual, _.message):
+                raise AssertionError('Expected {!r} {}'.format(_.actual,  _.message))
 
-            expect(callback).not_to.raise_error(AssertionError)
+        expect(callback).not_to.raise_error(AssertionError)
 
-        def it_should_pass_if_assertion_error_raised_and_pattern_matches():
-            def callback():
-                with failure(_.actual, _.pattern):
-                    raise AssertionError('Expected {!r} {}'.format(_.actual,  _.message))
+    def it_should_pass_if_assertion_error_raised_and_pattern_matches():
+        def callback():
+            with failure(_.actual, _.pattern):
+                raise AssertionError('Expected {!r} {}'.format(_.actual,  _.message))
 
-            expect(callback).not_to.raise_error(AssertionError)
+        expect(callback).not_to.raise_error(AssertionError)
 
-        def it_should_pass_if_assertion_error_raised_with_plugin_name_in_message():
-            def callback():
-                with failure(_.actual, _.pattern):
-                    raise AssertionError('Expected plugin {!r} {}'.format(_.actual,  _.message))
+    def it_should_pass_if_assertion_error_raised_with_plugin_name_in_message():
+        def callback():
+            with failure(_.actual, _.pattern):
+                raise AssertionError('Expected plugin {!r} {}'.format(_.actual,  _.message))
 
-            expect(callback).not_to.raise_error(AssertionError)
+        expect(callback).not_to.raise_error(AssertionError)
 
-        def it_should_fail_if_assertion_error_not_raised():
-            def callback():
-                with failure(_.actual, _.message):
-                    pass
+    def it_should_fail_if_assertion_error_not_raised():
+        def callback():
+            with failure(_.actual, _.message):
+                pass
 
-            expect(callback).to.raise_error(AssertionError)
+        expect(callback).to.raise_error(AssertionError)
 
-        def it_should_fail_if_assertion_error_raised_and_message_does_not_match():
-            def callback():
-                with failure(_.actual, _.message):
-                    raise AssertionError('foo')
+    def it_should_fail_if_assertion_error_raised_and_message_does_not_match():
+        def callback():
+            with failure(_.actual, _.message):
+                raise AssertionError('foo')
 
-            expect(callback).to.raise_error(AssertionError)
+        expect(callback).to.raise_error(AssertionError)
 
-        def it_should_fail_if_another_exception_raised():
-            def callback():
-                with failure(_.actual, _.message):
-                    raise KeyError()
+    def it_should_fail_if_another_exception_raised():
+        def callback():
+            with failure(_.actual, _.message):
+                raise KeyError()
 
-            expect(callback).to.raise_error(AssertionError)
+        expect(callback).to.raise_error(AssertionError)
 
-        @before.each
-        def fixtures():
-            _.actual = 'foo'
-            _.message = "to be 'bar'"
-            _.pattern = "to be '\w+'"
+    @before.each
+    def fixtures():
+        _.actual = 'foo'
+        _.message = "to be 'bar'"
+        _.pattern = "to be '\w+'"
