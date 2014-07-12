b63706db7040c7e9515cf3b724af20da5961919a
Jaime Gil de Sagredo Luna <jaimegildesagredo@gmail.com>
2014-07-12 20:46:14 +0200

    Fix specs

--- spec/testing/failure_spec.py
+++ spec/testing/failure_spec.py
@@ -12,21 +12,21 @@ with describe('failure') as _:
             with failure(_.actual, _.message):
                 raise AssertionError('Expected {!r} {}'.format(_.actual,  _.message))
 
-        expect(callback).not_to.raise_error(AssertionError)
+        expect(callback).not_to(raise_error(AssertionError))
 
     def it_should_pass_if_assertion_error_raised_and_pattern_matches():
         def callback():
             with failure(_.actual, _.pattern):
                 raise AssertionError('Expected {!r} {}'.format(_.actual,  _.message))
 
-        expect(callback).not_to.raise_error(AssertionError)
+        expect(callback).not_to(raise_error(AssertionError))
 
     def it_should_pass_if_assertion_error_raised_with_plugin_name_in_message():
         def callback():
             with failure(_.actual, _.pattern):
                 raise AssertionError('Expected plugin {!r} {}'.format(_.actual,  _.message))
 
-        expect(callback).not_to.raise_error(AssertionError)
+        expect(callback).not_to(raise_error(AssertionError))
 
     def it_should_pass_if_assertion_error_raised_and_only_message_matches():
         with failure(_.pattern):
@@ -37,21 +37,21 @@ with describe('failure') as _:
             with failure(_.actual, _.message):
                 pass
 
-        expect(callback).to.raise_error(AssertionError)
+        expect(callback).to(raise_error(AssertionError))
 
     def it_should_fail_if_assertion_error_raised_and_message_does_not_match():
         def callback():
             with failure(_.actual, _.message):
                 raise AssertionError('foo')
 
-        expect(callback).to.raise_error(AssertionError)
+        expect(callback).to(raise_error(AssertionError))
 
     def it_should_fail_if_another_exception_raised():
         def callback():
             with failure(_.actual, _.message):
                 raise KeyError()
 
-        expect(callback).to.raise_error(AssertionError)
+        expect(callback).to(raise_error(AssertionError))
 
     @before.each
     def fixtures():
