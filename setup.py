683a945e30e4ac26a681269b71ad2d9d3df728bd
Jaime Gil de Sagredo Luna <jaimegildesagredo@gmail.com>
2014-07-29 21:34:51 +0200

    py 3.4 also supported ;)

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'License :: OSI Approved :: Apache Software License'
