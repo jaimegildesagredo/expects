b960d84465803a3a4c72569351e4dbf3898e7424
Jaime Gil de Sagredo Luna <jaimegildesagredo@gmail.com>
2014-08-13 20:30:32 +0200

    Format

--- specs/matchers/start_with_spec.py
+++ specs/matchers/start_with_spec.py
@@ -15,66 +15,66 @@ with describe('start_with'):
         self.dct = {'bar': 0, 'baz': 1}
         self.ordered_dct = OrderedDict([('bar', 0), ('baz', 1)])
 
-    with it('should_pass_if_string_starts_with_string'):
+    with it('should pass if string starts with string'):
         expect(self.str).to(start_with(self.str[:5]))
 
-    with it('should_pass_if_list_starts_with_arg'):
+    with it('should pass if list starts with arg'):
         expect(self.lst).to(start_with(self.lst[0]))
 
-    with it('should_pass_if_list_starts_with_args'):
+    with it('should pass if list starts with args'):
         expect(self.lst).to(start_with(*self.lst[:2]))
 
-    with it('should_pass_if_ordered_dict_starts_with_keys'):
+    with it('should pass if ordered dict starts with keys'):
         expected_args = list(self.ordered_dct)[:2]
 
         expect(self.ordered_dct).to(start_with(*expected_args))
 
-    with it('should_pass_if_iter_starts_with_args'):
+    with it('should pass if iter starts with args'):
         expect(iter(self.lst)).to(start_with(*self.lst[:2]))
 
-    with it('should_fail_if_string_does_not_start_with_string'):
+    with it('should fail if string does not start with string'):
         expected = self.str[5:]
 
         with failure('to start with {!r}'.format(expected)):
             expect(self.str).to(start_with(expected))
 
-    with it('should_fail_if_list_does_not_start_with_arg'):
+    with it('should fail if list does not start with arg'):
         expected = self.lst[1]
 
         with failure('to start with {!r}'.format(expected)):
             expect(self.lst).to(start_with(expected))
 
-    with it('should_fail_if_list_does_not_start_with_args'):
+    with it('should fail if list does not start with args'):
         expected_args = self.lst[1:]
 
         with failure('to start with {!r} and {!r}'.format(*expected_args)):
             expect(self.lst).to(start_with(*expected_args))
 
-    with it('should_fail_if_list_starts_with_first_arg_but_not_second'):
+    with it('should fail if list starts with first arg but not second'):
         expected_args = self.lst[0], self.lst[0]
 
         with failure('to start with {!r} and {!r}'.format(*expected_args)):
             expect(self.lst).to(start_with(*expected_args))
 
-    with it('should_fail_if_actual_is_a_dict'):
+    with it('should fail if actual is a dict'):
         with failure('to start with {!r} and {!r} '
                      'but it does not have ordered keys'.format(*IRRELEVANT_ARGS)):
 
             expect(self.dct).to(start_with(*IRRELEVANT_ARGS))
 
     with context('#negated'):
-        with it('should_pass_if_string_does_not_start_with_string'):
+        with it('should pass if string does not start with string'):
             expect(self.str).not_to(start_with(self.str[5:]))
 
-        with it('should_pass_if_list_does_not_start_with_args'):
+        with it('should pass if list does not start with args'):
             expect(self.lst).not_to(start_with(*self.lst[2:]))
 
-        with it('should_pass_if_list_starts_with_first_arg_but_not_second'):
+        with it('should pass if list starts with first arg but not second'):
             expected_args = self.lst[0], self.lst[0]
 
             expect(self.lst).not_to(start_with(*expected_args))
 
-        with it('should_fail_if_actual_is_a_dict'):
+        with it('should fail if actual is a dict'):
             with failure('not to start with {!r} and {!r} '
                          'but it does not have ordered keys'.format(*IRRELEVANT_ARGS)):
 
