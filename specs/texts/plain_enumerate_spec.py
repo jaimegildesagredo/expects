874cbebb465781ab39e95badbcb173ebff3c9216
Jaime Gil de Sagredo Luna <jaimegildesagredo@gmail.com>
2014-08-16 17:44:25 +0200

    More examples

--- specs/texts/plain_enumerate_spec.py
+++ specs/texts/plain_enumerate_spec.py
@@ -6,13 +6,13 @@ from expects.texts import plain_enumerate
 
 with describe('plain_enumerate'):
     with context('one arg'):
-        with it('returns representation of arg'):
+        with it('returns repr of arg'):
             result = plain_enumerate(('foo',))
 
             expect(result).to(equal("'foo'"))
 
     with context('two args'):
-        with it('returns representation of args'):
+        with it('returns repr of args'):
             result = plain_enumerate(('foo', 'bar'))
 
             expect(result).to(equal("'foo' and 'bar'"))
@@ -28,3 +28,10 @@ with describe('plain_enumerate'):
             result = plain_enumerate(('foo',), {'a': 0})
 
             expect(result).to(equal("'foo' and 'a' equal 0"))
+
+    with context('three args and three kwargs'):
+        with it('returns repr of args and kwargs'):
+            result = plain_enumerate(('foo', 'bar', 'baz'), {'a': 0, 'b': 1, 'c': 2})
+
+            expect(result).to(equal("'foo', 'bar', 'baz', 'a' equal 0, "
+                                    "'b' equal 1 and 'c' equal 2"))
