e12efcc859a6cadfa392021d6968ebe38e3b981d
Jaime Gil de Sagredo Luna <jaimegildesagredo@gmail.com>
2014-12-04 20:59:28 +0100

    Shorter example names

--- specs/matchers/built_in/have_len_spec.py
+++ specs/matchers/built_in/have_len_spec.py
@@ -4,25 +4,25 @@ from expects import *
 from expects.testing import failure
 
 
-with describe('have_length'):
-    with it('should pass if string has the expected length'):
+with describe('have_len'):
+    with it('passes if string has the expected length'):
         expect('foo').to(have_len(3))
 
-    with it('should fail if string does not have the expected length'):
+    with it('passes if iterable has the expected length'):
+        expect(iter('foo')).to(have_len(3))
+
+    with it('fails if string does not have the expected length'):
         with failure("Expected 'foo' to have len 2"):
             expect('foo').to(have_len(2))
 
-    with it('should pass if iterable has the expected length'):
-        expect(iter('foo')).to(have_len(3))
-
-    with it('should fail if iterable does not have the expected length'):
+    with it('fails if iterable does not have the expected length'):
         with failure('to have len 2'):
             expect(iter('foo')).to(have_len(2))
 
     with context('#negated'):
-        with it('should pass if string does not have the expected length'):
+        with it('passes if string does not have the expected length'):
             expect('foo').not_to(have_len(2))
 
-        with it('should fail if string has the expected length'):
+        with it('fails if string has the expected length'):
             with failure("Expected 'foo' not to have len 3"):
                 expect('foo').not_to(have_len(3))
--- specs/matchers/built_in/have_length_spec.py
+++ specs/matchers/built_in/have_length_spec.py
@@ -5,24 +5,24 @@ from expects.testing import failure
 
 
 with describe('have_length'):
-    with it('should pass if string has the expected length'):
+    with it('passes if string has the expected length'):
         expect('foo').to(have_length(3))
 
-    with it('should fail if string does not have the expected length'):
+    with it('passes if iterable has the expected length'):
+        expect(iter('foo')).to(have_length(3))
+
+    with it('fails if string does not have the expected length'):
         with failure("Expected 'foo' to have length 2"):
             expect('foo').to(have_length(2))
 
-    with it('should pass if iterable has the expected length'):
-        expect(iter('foo')).to(have_length(3))
-
-    with it('should fail if iterable does not have the expected length'):
+    with it('fails if iterable does not have the expected length'):
         with failure('to have length 2'):
             expect(iter('foo')).to(have_length(2))
 
     with context('#negated'):
-        with it('should pass if string does not have the expected length'):
+        with it('passes if string does not have the expected length'):
             expect('foo').not_to(have_length(2))
 
-        with it('should fail if string has the expected length'):
+        with it('fails if string has the expected length'):
             with failure("Expected 'foo' not to have length 3"):
                 expect('foo').not_to(have_length(3))
