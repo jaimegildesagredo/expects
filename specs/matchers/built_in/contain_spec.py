523f8f035e08d36c18de93bd195a6baec1390dfe
Jaime Gil de Sagredo Luna <jaimegildesagredo@gmail.com>
2014-12-09 22:30:13 +0100

    Examples rename

--- specs/matchers/built_in/contain_spec.py
+++ specs/matchers/built_in/contain_spec.py
@@ -10,59 +10,59 @@ with describe('contain'):
         self.itr = iter(self.lst)
         self.str = 'My foo string'
 
-    with it('should pass if list contains item'):
+    with it('passes if list contains item'):
         expect(self.lst).to(contain('bar'))
 
-    with it('should pass if list contains items'):
+    with it('passes if list contains items'):
         expect(self.lst).to(contain('bar', 'baz'))
 
-    with it('should pass if iterable of dicts contains dict'):
+    with it('passes if iterable of dicts contains dict'):
         # https://github.com/jaimegildesagredo/expects/issues/8
 
         expect([{'foo': 1}, 'bar']).to(contain({'foo': 1}))
 
-    with it('should pass if iterable contains item'):
+    with it('passes if iterable contains item'):
         expect(self.itr).to(contain('bar'))
 
-    with it('should pass if iterable contains items'):
+    with it('passes if iterable contains items'):
         expect(self.itr).to(contain('bar', 'baz'))
 
-    with it('should pass if string contains string'):
+    with it('passes if string contains string'):
         expect(self.str).to(contain('foo'))
 
-    with it('should pass if string contains strings'):
+    with it('passes if string contains strings'):
         expect(self.str).to(contain('foo', 'string'))
 
-    with it('should fail if list does not contain item'):
+    with it('fails if list does not contain item'):
         with failure("to contain 'bar' and 'foo'"):
             expect(self.lst).to(contain('bar', 'foo'))
 
-    with it('should fail if iterable does not contain item'):
+    with it('fails if iterable does not contain item'):
         with failure(end_with("to contain 'bar' and 'foo'")):
             expect(self.itr).to(contain('bar', 'foo'))
 
-    with it('should fail if is not an iterable object'):
+    with it('fails if is not an iterable object'):
         with failure("to contain 'bar' but is not a valid sequence type"):
             expect(object()).to(contain('bar'))
 
     with context('#negated'):
-        with it('should pass if list does not contain item'):
+        with it('passes if list does not contain item'):
             expect(self.lst).not_to(contain('foo'))
 
-        with it('should pass if list does not contain items'):
+        with it('passes if list does not contain items'):
             expect(self.lst).not_to(contain('foo', 'foobar'))
 
-        with it('should pass if list contains one item and not the other'):
+        with it('passes if list contains one item and not the other'):
             expect(self.lst).not_to(contain('bar', 'foo'))
 
-        with it('should fail if list contains item'):
+        with it('fails if list contains item'):
             with failure("not to contain 'bar'"):
                 expect(self.lst).not_to(contain('bar'))
 
-        with it('should fail if list contains items'):
+        with it('fails if list contains items'):
             with failure("not to contain 'bar' and 'baz'"):
                 expect(self.lst).not_to(contain('bar', 'baz'))
 
-        with it('should fail if is not an iterable object'):
+        with it('fails if is not an iterable object'):
             with failure("not to contain 'bar' but is not a valid sequence type"):
                 expect(object()).not_to(contain('bar'))
