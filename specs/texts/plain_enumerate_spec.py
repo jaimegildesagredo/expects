ea1864ed1b6d782a07b91a63eced82b7956e8c20
Jaime Gil de Sagredo Luna <jgil@alea-soluciones.com>
2015-07-13 10:36:13 +0200

    Improve test names

--- specs/texts/plain_enumerate_spec.py
+++ specs/texts/plain_enumerate_spec.py
@@ -5,44 +5,44 @@ from expects.texts import plain_enumerate
 
 
 with describe('plain_enumerate'):
-    with context('one arg'):
+    with context('with one arg'):
         with it('returns repr of arg'):
             result = plain_enumerate(('foo',))
 
             expect(result).to(equal("'foo'"))
 
-    with context('two args'):
-        with it('returns repr of args'):
+    with context('with two args'):
+        with it('returns repr of args joined by "and"'):
             result = plain_enumerate(('foo', 'bar'))
 
             expect(result).to(equal("'foo' and 'bar'"))
 
-    with context('three args'):
-        with it('returns repr of args'):
+    with context('with three args'):
+        with it('returns repr of args joined by "," and "and"'):
             result = plain_enumerate(('foo', 'bar', 'baz'))
 
             expect(result).to(equal("'foo', 'bar' and 'baz'"))
 
-    with context('one arg and one kwarg'):
-        with it('returns repr of args and kwargs'):
+    with context('with one arg and one kwarg'):
+        with it('returns repr of arg and kwarg joined by "and"'):
             result = plain_enumerate(('foo',), {'a': 0})
 
             expect(result).to(equal("'foo' and 'a' equal 0"))
 
-    with context('three args and three kwargs'):
-        with it('returns repr of args and kwargs'):
+    with context('with three args and three kwargs'):
+        with it('returns repr of args and kwargs joined by "," and "and"'):
             result = plain_enumerate(('foo', 'bar', 'baz'), {'a': 0, 'b': 1, 'c': 2})
 
             expect(result).to(equal("'foo', 'bar', 'baz', 'a' equal 0, "
                                     "'b' equal 1 and 'c' equal 2"))
 
-    with context('one matcher'):
+    with context('with one matcher'):
         with it('returns matcher description'):
             result = plain_enumerate((equal(1),))
 
             expect(result).to(equal("equal 1"))
 
-    with context('any object with a _description method'):
+    with context('with any object with a _description method'):
         # https://github.com/jaimegildesagredo/expects/issues/26
         # https://github.com/jaimegildesagredo/doublex-expects/issues/8
 
