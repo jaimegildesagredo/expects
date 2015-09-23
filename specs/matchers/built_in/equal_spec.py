d83736b018ec5d30945c01a328e74889acc1108f
alejandrodob <alejandrodob@gmail.com>
2015-09-23 18:48:11 +0200

    Equal matcher for inequality using !=

--- specs/matchers/built_in/equal_spec.py
+++ specs/matchers/built_in/equal_spec.py
@@ -19,3 +19,28 @@ with describe('equal'):
         with it('should fail if number equals expected'):
             with failure('expected: 1 not to equal 1'):
                 expect(1).not_to(equal(1))
+
+    with context('when comparing objects'):
+        with before.each:
+            class Foo(object):
+                compared = False
+
+                def __eq__(self, other):
+                    self.compared = True
+                    return True
+
+                def __ne__(self, other):
+                    self.compared = True
+                    return True
+
+            self.an_object = Foo()
+            self.another_object = Foo()
+
+        with it('should be computed based on the object __eq__ method'):
+            expect(self.an_object).to(equal(self.another_object))
+            expect(self.an_object.compared).to(equal(True))
+
+        with context('#negated'):
+            with it('should be computed based on the object __ne__ method'):
+                expect(self.an_object).not_to(equal(self.another_object))
+                expect(self.an_object.compared).to(equal(True))
