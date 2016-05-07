7fdba83ecae7b10845b8e1bc8f79c15c8c758285
Tom Godkin <thgodkin@gmail.com>
2016-05-07 21:02:49 +0100

    allow matching tolerance for equal

--- expects/matchers/built_in/equal.py
+++ expects/matchers/built_in/equal.py
@@ -12,3 +12,19 @@ class equal(Matcher):
 
     def _match_negated(self, subject):
         return subject != self._expected, []
+
+    def within(self, tolerance):
+        return equal_within(self._expected, tolerance)
+
+
+class equal_within(Matcher):
+    def __init__(self, expected, tolerance):
+        self._expected = expected
+        self._tolerance = tolerance
+
+    def _match(self, subject):
+        difference = abs(self._expected - subject)
+        return difference < self._tolerance, []
+
+    def __repr__(self):
+        return 'equal {0} ± {1}'.format(self._expected, self._tolerance)
