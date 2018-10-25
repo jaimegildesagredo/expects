e66230bc7903e6aa01b312baa5edee8392362f52
Jaime Gil de Sagredo Luna <jaimegildesagredo@gmail.com>
2018-10-25 14:12:59 +0200

    Dropped Python 2.6 support

--- setup.py
+++ setup.py
@@ -21,9 +21,11 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'License :: OSI Approved :: Apache Software License'
