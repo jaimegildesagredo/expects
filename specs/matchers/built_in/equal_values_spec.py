96758a857e4db5e7860b8fe2d681f0e32fe60190
Daniel Baena <dbaena@alea-soluciones.com>
2019-04-29 16:07:15 +0200

    Add equal_values matcher for comparing object attributes

---
+++ specs/matchers/built_in/equal_values_spec.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*
+
+from expects import *
+from expects.testing import failure
+
+
+with describe('equal values'):
+    with context('when comparing objects'):
+        with before.each:
+            class Foo(object):
+                def __init__(self, bar):
+                    self.bar = bar
+
+            self.object = Foo(1)
+            self.another_object = Foo(1)
+            self.object_with_crazy_logic = Foo('crazy')
+
+        with it('should pass if object values does not equal expected object values'):
+            expect(self.object).not_to(equal_values(self.object_with_crazy_logic))
+
+        with it('should pass if object equals expected'):
+            expect(self.object).to(equal_values(self.another_object))
