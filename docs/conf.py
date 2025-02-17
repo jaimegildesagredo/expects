67d07ae30850d1c28bb1a312027968e996e76843
Jaime Gil de Sagredo Luna <jaimegildesagredo@gmail.com>
2025-02-18 00:18:47 +0100

    Fix Sphinx conf

--- docs/conf.py
+++ docs/conf.py
@@ -15,12 +15,10 @@
 import sys
 import os
 
-import expects
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.dirname(expects.__file__))
+sys.path.insert(0, os.path.dirname(".."))
 
 # -- General configuration ------------------------------------------------
 
@@ -104,7 +102,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -265,4 +263,6 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'http://docs.python.org/': None}
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3/", None),
+}
